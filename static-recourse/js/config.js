a2d0ef40777ab2204c34df0b4aee9ed476d0caea
LiXiaoHui <1628567324@qq.com>
2019-01-10 17:47:27 +0800

    提出IP配置

---
+++ static-recourse/js/config.js
@@ -0,0 +1,9 @@
+const ip="http://10.0.0.44:8083/";
+const seal_type = [
+    { name: '单位专用印章', value: '01' },
+    { name: '财务专用章', value: '02' },
+    { name: '税务专用章', value: '03' },
+    { name: '合同专用章', value: '04' },
+    { name: '法定代表人名章', value: '05' },
+    { name: '其他类型印章', value: '99' }
+  ];
